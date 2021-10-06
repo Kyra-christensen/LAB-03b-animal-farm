d02a6c51010c900c55e733ff3c34a79cc716171b
Kyra Christensen <christensen.kyral@gmail.com>
2021-10-06 16:10:03 -0700

    add animal images and associates sounds to them

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 // import functions
 
 // reference needed DOM elements
+const catImage = document.getElementById('cat-image');
+const catSound = document.getElementById('cat-sound');
+const dogImage = document.getElementById('dog-image');
+const dogSound = document.getElementById('dog-sound');
+const horseImage = document.getElementById('horse-image');
+const horseSound = document.getElementById('horse-sound');
 
+let catClicks = 0;
+let dogClicks = 0;
+let horseClicks = 0;
 
 // set event listeners 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+catImage.addEventListener('click', () => {
+    catSound.play();
+    catClicks = catClicks + 1;
+    console.log('cat clicks is now', catClicks);
+});
+
+dogImage.addEventListener('click', () => {
+    dogSound.play();
+    dogClicks = dogClicks + 1;
+    console.log('dog clicks is now', dogClicks);
+});
+
+horseImage.addEventListener('click', () => {
+    horseSound.play();
+    horseClicks = horseClicks + 1;
+    console.log('horse clicks is now', horseClicks);
+});
